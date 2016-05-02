9ff247f2e9e2b3cb3159b09be41ce446d9d431f6
Gui Iribarren <gui@altermundi.net>
2016-05-02 22:47:19 +0100

    update configuration of babeld based on what was used at wbmv8 manual testbed

--- packages/wibed-battlemesh-experiment/files/etc/wbm/protos/babel.sh
+++ packages/wibed-battlemesh-experiment/files/etc/wbm/protos/babel.sh
@@ -23,18 +23,37 @@ prepare () {
 
   touch /etc/config/babeld
 
-  uci add babeld filter
-  uci set babeld.@filter[-1].ignore=false
-  uci set babeld.@filter[-1].type=redistribute
-  uci set babeld.@filter[-1].local=1
-  uci set babeld.@filter[-1].action=deny
+  uci import babeld <<EOF
 
-  uci commit babeld
+config general
+  option 'log_file' '/var/log/babeld.log'
+  option 'ipv6_subtrees' 'true'
+  option 'diversity' '3'
+
+config filter
+  option ignore 'false'
+  option type 'redistribute'
+  option local '1'
+  option action 'deny'
+
+config interface
+  option 'enable_timestamps' 'true'
+
+config filter
+  option 'type' 'redistribute'
+  option 'action' 'allow'
+
+config filter
+  option 'ignore' 'true'
+  option 'type' 'redistribute'
+  option 'local'  'true'
+
+EOF
 }
 
 add () {
   uci set babeld.${LOGICAL_INTERFACE}=interface
-  uci set babeld.${LOGICAL_INTERFACE}.ignore=false
+  uci set babeld.${LOGICAL_INTERFACE}.ifname=${LOGICAL_INTERFACE}
 
   uci add babeld filter
   uci set babeld.@filter[-1].ignore=false
@@ -45,12 +64,12 @@ add () {
 }
 
 start () {
-	/etc/init.d/babeld start
+  /etc/init.d/babeld start
 }
 
 stop () {
-	/etc/init.d/babeld stop
-	killall -9 babeld 2>/dev/null
+  /etc/init.d/babeld stop
+  killall -9 babeld 2>/dev/null
 }
 
 $ACTION
