392bf8ef64c1c0f580dae6ba764f573f19f82010
Gui Iribarren <gui@altermundi.net>
2016-05-03 18:32:31 +0100

    change bmx6 namespace to bmx7

--- packages/wibed-battlemesh-experiment/files/etc/wbm/protos/bmx7.sh
+++ packages/wibed-battlemesh-experiment/files/etc/wbm/protos/bmx7.sh
@@ -3,7 +3,7 @@
 #set -x
 
 #ACTIONS are: add prepare clean?
-#example: bmx6.sh <action> <virtual_if> <actual_if> <IPv4/16> <IPv6/64>
+#example: bmx7.sh <action> <virtual_if> <actual_if> <IPv4/16> <IPv6/64>
 
 ACTION=$1
 LOGICAL_INTERFACE=$2
@@ -12,94 +12,94 @@ IPV4=$4
 IPV6=$5
 
 clean () {
-  uci revert bmx6
-  rm /etc/config/bmx6
-  touch /etc/config/bmx6
+  uci revert bmx7
+  rm /etc/config/bmx7
+  touch /etc/config/bmx7
 }
 
 prepare () {
-  rm -f /etc/config/bmx6
-  touch /etc/config/bmx6
+  rm -f /etc/config/bmx7
+  touch /etc/config/bmx7
 
-  uci set bmx6.general=bmx6
-# uci set bmx6.general.ipAutoPrefix="::/0"
-# uci set bmx6.general.globalPrefix="fd11::/48"
+  uci set bmx7.general=bmx7
+# uci set bmx7.general.ipAutoPrefix="::/0"
+# uci set bmx7.general.globalPrefix="fd11::/48"
 
   # Prevent syslog messages by default
-  uci set bmx6.general.syslog=0
+  uci set bmx7.general.syslog=0
 
   # Some tunning for the WBM scenario
-  uci set bmx6.general.dbgMuteTimeout=1000000
-# uci set bmx6.general.purgeTimeout=70000
-# uci set bmx6.general.linkPurgeTimeout=20000
-# uci set bmx6.general.dadTimeout=15000
+  uci set bmx7.general.dbgMuteTimeout=1000000
+# uci set bmx7.general.purgeTimeout=70000
+# uci set bmx7.general.linkPurgeTimeout=20000
+# uci set bmx7.general.dadTimeout=15000
 
-  # Enable bmx6 uci config plugin
-  uci set bmx6.config=plugin
-  uci set bmx6.config.plugin=bmx6_config.so
+  # Enable bmx7 uci config plugin
+  uci set bmx7.config=plugin
+  uci set bmx7.config.plugin=bmx7_config.so
 
-  # Enable de JSON plugin to get bmx6 information in json format
-#  uci set bmx6.json=plugin
-#  uci set bmx6.json.plugin=bmx6_json.so
+  # Enable de JSON plugin to get bmx7 information in json format
+#  uci set bmx7.json=plugin
+#  uci set bmx7.json.plugin=bmx7_json.so
 
   # Disable ThrowRules because they are broken in IPv6 with current Linux Kernel
-  uci set bmx6.ipVersion=ipVersion
-  uci set bmx6.ipVersion.ipVersion=6
-  uci set bmx6.ipVersion.throwRules=0
+  uci set bmx7.ipVersion=ipVersion
+  uci set bmx7.ipVersion.ipVersion=6
+  uci set bmx7.ipVersion.throwRules=0
 
 
   # Smart gateway search for IPV4
 
   # Search for any announcement of 10/8 in the mesh cloud
-  #uci set bmx6.mesh=tunOut
-  #uci set bmx6.mesh.tunOut=mesh
-  #uci set bmx6.mesh.network=10.0.0.0/8
-  #uci set bmx6.mesh.minPrefixLen=24
-  #uci set bmx6.mesh.maxPrefixLen=32
+  #uci set bmx7.mesh=tunOut
+  #uci set bmx7.mesh.tunOut=mesh
+  #uci set bmx7.mesh.network=10.0.0.0/8
+  #uci set bmx7.mesh.minPrefixLen=24
+  #uci set bmx7.mesh.maxPrefixLen=32
 
   # Search for internet in the mesh cloud
-  #uci set bmx6.inet=tunOut
-  #uci set bmx6.inet.tunOut=inet
-  #uci set bmx6.inet.network=0.0.0.0/0
-  #uci set bmx6.inet.minPrefixLen=0
-  #uci set bmx6.inet.maxPrefixLen=0
+  #uci set bmx7.inet=tunOut
+  #uci set bmx7.inet.tunOut=inet
+  #uci set bmx7.inet.network=0.0.0.0/0
+  #uci set bmx7.inet.minPrefixLen=0
+  #uci set bmx7.inet.maxPrefixLen=0
 
 
 # Smart gateway search for IPV6
   
   # Search for any IPv6 announcement in the mesh cloud
-  #uci set bmx6.ipv6=tunOut
-  #uci set bmx6.ipv6.tunOut=ipv6
-  #uci set bmx6.ipv6.network=::/0
+  #uci set bmx7.ipv6=tunOut
+  #uci set bmx7.ipv6.tunOut=ipv6
+  #uci set bmx7.ipv6.network=::/0
 
-  uci commit bmx6
+  uci commit bmx7
 }
 
 add () {
-  uci set bmx6.${LOGICAL_INTERFACE}=dev
-  uci set bmx6.${LOGICAL_INTERFACE}.dev=${REAL_INTERFACE}
-  uci set bmx6.${LOGICAL_INTERFACE}.globalPrefix="$( echo ${IPV6} echo | sed s/"\/.*"/"\/128"/ )"
+  uci set bmx7.${LOGICAL_INTERFACE}=dev
+  uci set bmx7.${LOGICAL_INTERFACE}.dev=${REAL_INTERFACE}
+  uci set bmx7.${LOGICAL_INTERFACE}.globalPrefix="$( echo ${IPV6} echo | sed s/"\/.*"/"\/128"/ )"
 
   # To enable IPv4
 
-  #if uci -q get bmx6.general.tun4Address > /dev/null ; then
-  #  uci set bmx6.tun_${LOGICAL_INTERFACE}=tunInNet
-  #  uci set bmx6.tun_${LOGICAL_INTERFACE}.tunInNet="$( echo ${IPV4} echo | sed s/"\/.*"/"\/32"/ )"
-  #  uci set bmx6.tun_${LOGICAL_INTERFACE}.bandwidth="128000000000"
+  #if uci -q get bmx7.general.tun4Address > /dev/null ; then
+  #  uci set bmx7.tun_${LOGICAL_INTERFACE}=tunInNet
+  #  uci set bmx7.tun_${LOGICAL_INTERFACE}.tunInNet="$( echo ${IPV4} echo | sed s/"\/.*"/"\/32"/ )"
+  #  uci set bmx7.tun_${LOGICAL_INTERFACE}.bandwidth="128000000000"
   #else
-  #  uci set bmx6.general.tun4Address="$( echo ${IPV4} echo | sed s/"\/.*"/"\/32"/ )"
+  #  uci set bmx7.general.tun4Address="$( echo ${IPV4} echo | sed s/"\/.*"/"\/32"/ )"
   #fi
 
-  uci commit bmx6
+  uci commit bmx7
 }
 
 stop () {
-  /etc/init.d/bmx6 stop
-  killall -9 bmx6 2>/dev/null
+  /etc/init.d/bmx7 stop
+  killall -9 bmx7 2>/dev/null
 }
 
 start () {
-  /etc/init.d/bmx6 start
+  /etc/init.d/bmx7 start
 }
 
 $ACTION
