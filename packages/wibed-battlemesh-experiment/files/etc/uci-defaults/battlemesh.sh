975ae5ff807417ae300db86310621ca57cecb1d5
Gui Iribarren <gui@altermundi.net>
2016-05-06 04:52:48 +0100

    disable all protocols by default (including batadv). also don't use vlans

--- packages/wibed-battlemesh-experiment/files/etc/uci-defaults/battlemesh.sh
+++ packages/wibed-battlemesh-experiment/files/etc/uci-defaults/battlemesh.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
 /usr/bin/wbm-config > /tmp/wbm-config.log 2>&1
+
+### Disable all protocols by default (including batadv)
+### and point them to the raw wifi interface (no vlans)
+for proto in babel bmx7 olsr olsr2 batadv ; do
+    uci set network.wbm1_$proto.ifname='@wbm1_base'
+    uci set network.wbm1_$proto.disabled=1
+    uci set network.lan_$proto.disabled=1
+done
+for daemon in babeld olsrd olsrd6 olsrd2 bmx7 ; do
+    /etc/init.d/$daemon disable
+done
